bfd2e52a197e9d04f585624c4818119156420ae7
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-01-19 18:29:45 -0500

    Define Data Types

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,16 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+# x = 1             #int
+# y = 2.5           #float
+# name = 'Suprem'   #str
+# is_cool = True    #bool
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, "Suprem", True)
+
+# Casting
+x = str(x)
+
+print(type(x))
