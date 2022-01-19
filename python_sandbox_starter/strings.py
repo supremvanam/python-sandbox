73cc7acaca4aa8ef6a5d27d68fde5001475ed22d
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-01-19 18:42:22 -0500

    String Methods

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -5,12 +5,19 @@ age = 25
 # Concatenate
 print("Hello, my name is " + name + " and I am " + str(age))
 
+# String Formatting
+
 # Arguments by position
 print("My name is {n}, {a} years old".format(n = name, a = age))
 
 # f-strings
 print(f"Hello this is {name} and I am {age}")
 
-# String Formatting
-
 # String Methods
+
+s = "helloworld"
+
+print(s.split())
+
+print(s.isalnum())
+
