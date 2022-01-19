66efad42731af387b2531a3f2474ca9dbd3a7d57
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-01-19 18:36:36 -0500

    String Formatting

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,5 +1,15 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
+name = "Suprem"
+age = 25
 
+# Concatenate
+print("Hello, my name is " + name + " and I am " + str(age))
+
+# Arguments by position
+print("My name is {n}, {a} years old".format(n = name, a = age))
+
+# f-strings
+print(f"Hello this is {name} and I am {age}")
 
 # String Formatting
 
