657258637a5aa94e9f112a158d8d624e6bcb567a
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-01-19 19:45:39 -0500

    Created Lab 1 file

---
+++ python_sandbox_starter/lab1.py
@@ -0,0 +1,9 @@
+#imports and variables
+from random import randint
+
+#the following variable will be used in the rest of the exercise
+pm = 'justin pierre james trudeau'
+instructor = 'narendra pershad'
+harry = "You've gotta ask yourself a question: do I feel lucky? …well, do ya, punk?"
+numbers = [randint(5, 10) for _ in range(20)]
+
--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,16 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create a list
+numbers = [1,2,3,4,5]
+fruits = ["Apples", "Oranges", "Avacados", "Grapes", "Pears"]
+
+# Use a constructor
+numbers2 = list((6,7,8,9,10))
+
+print(fruits[1])
+print(len(fruits))
+fruits.append("Mangos")
+fruits.insert(2, "Strawberries")
+fruits.sort()
+
+print(fruits)
