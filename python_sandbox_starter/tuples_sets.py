f7e0b7f1d7766545ac9f72dc4f631e57540b9ab8
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-01-19 20:30:24 -0500

    Tuple Intro

--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,12 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
  
+# Create a Tuple
+fruits = ('Apples', 'Oranges', 'Grapes')
 
+print(fruits)
+
+print(fruits[1])
+# del fruits
+print(len(fruits))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
