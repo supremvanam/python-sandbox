0075ade18d065eb4961db3996df1da2a385ba8be
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-01-26 18:43:33 -0500

    Functions wrap up

--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -1,6 +1,13 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
+def sayHello(name = "suprem"):
+    print(f"hello {name}")
 
+sayHello()
 
+def getSum (num):
+    return num * num
+
+print(getSum(7))
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
